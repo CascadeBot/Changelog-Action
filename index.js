9084046158fa84a69cc332a23784f3e6a2ae1929
mrjvs <jellevs@gmail.com>
2019-11-26 00:14:23 +0100

    Functional github action. still needs formatting tho

--- index.js
+++ index.js
@@ -1,32 +1,60 @@
 const core = require('@actions/core');
 const axios = require('axios');
 
+require('dotenv').config();
+
 const { GITHUB_EVENT_NAME, GITHUB_EVENT_PATH } = process.env;
 
-async function run() {
-  const webhook = core.getInput('discordWebhook');
-  const authorisedUsers = core.getInput('authorisedUsers').split(',');
-
-  if (GITHUB_EVENT_NAME != 'issue_comment')
-    return core.setFailed('Not a comment');
-  const event = require(GITHUB_EVENT_PATH);
-  if (typeof event.pull_request !== 'undefined')
-    return core.setFailed('Not a PR');
-  if (!authorisedUsers.includes(event.user.login))
-    return core.setFailed('Unauthorised!');
-  
+function sendWebhook(event, webhook, color) {
   axios({
     method: 'post',
     url: webhook,
     data: {
+      avatar_url: event.organization.avatar_url,
+      username: event.organization.login,
       embeds: [{
         title: event.issue.title,
-        description: event.issue.body
+        description: event.issue.body,
+        color
       }]
     }
   }).catch(error => {
-    core.setFailed(error);
+    core.setFailed(error.message);
+  }).then(res => {
+    core.info('Command successfully ran!');
   });
 }
 
-run();
+async function run() {
+  const publishWebhook = core.getInput('discordPublishWebhook');
+  const previewWebhook = core.getInput('discordPreviewWebhook');
+  const authorisedUsers = core.getInput('authorisedUsers').split(',');
+  
+  if (GITHUB_EVENT_NAME != 'issue_comment')
+  return core.setFailed('Not a comment');
+
+  let event;
+  try {
+    event = require(GITHUB_EVENT_PATH);
+  } catch (error) {
+    return core.setFailed('Invalid event file: \'' + GITHUB_EVENT_PATH + '\'');
+  }
+
+  if (typeof event.pull_request !== 'undefined')
+    return core.setFailed('Not a PR');
+
+  if (!authorisedUsers.includes(event.comment.user.login))
+    return core.setFailed('Unauthorised!');
+  
+  if (event.comment.body.startsWith(';preview'))
+    sendWebhook(event, previewWebhook, 15158332);
+  else if (event.comment.body.startsWith(';publish'))
+    sendWebhook(event, publishWebhook, 3447003);
+  else {
+    core.setFailed('Not a command');
+  }
+}
+
+run().catch(error => {
+  core.setFailed('Event failed: ' + error.message);
+});
